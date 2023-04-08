3508d626df440aceb82897bd89efc5bd56861283
Sarangthem Amarjit Meetei <sarangthemamarjit123@gmail.com>
2023-04-08 16:10:00 +0530

    committed

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -17,6 +17,7 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       body: Center(
         child: Card(
+          color: Colors.grey[50],
           elevation: 20,
           child: SizedBox(
             height: height / 1.8,
@@ -27,128 +28,132 @@ class _LoginPageState extends State<LoginPage> {
                     flex: 1,
                     child: Container(
                       color: Colors.grey[50],
+                      height: height / 4,
                       child: Padding(
                         padding: const EdgeInsets.only(left: 40.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                            SizedBox(
-                                height: 90,
-                                child: Image.asset(
-                                    "assets/images/G-png-only.png")),
-                            const Padding(
-                              padding: EdgeInsets.symmetric(vertical: 8.0),
-                              child: Text(
-                                "Leave Management System",
-                                style: TextStyle(
-                                    fontSize: 18, fontWeight: FontWeight.w400),
+                        child: SizedBox(
+                          // color: Colors.red,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              SizedBox(
+                                  height: 90,
+                                  child: Image.asset(
+                                      "assets/images/G-png-only.png")),
+                              const Padding(
+                                padding: EdgeInsets.symmetric(vertical: 8.0),
+                                child: Text(
+                                  "Leave Management System",
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w400),
+                                ),
                               ),
-                            ),
-                            const Text(
-                              "Admin Panel",
-                              style: TextStyle(
-                                  fontSize: 18, fontWeight: FontWeight.bold),
-                            )
-                          ],
+                              const Text(
+                                "Admin Panel",
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              )
+                            ],
+                          ),
                         ),
                       ),
                     )),
                 Expanded(
                     flex: 1,
                     child: Container(
-                      color: const Color.fromARGB(255, 196, 28, 28),
-                      child: Column(children: [
-                        Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 24.0),
-                          child: Align(
-                            alignment: Alignment.center,
-                            child: Container(
-                              child: Column(
-                                children: [
-                                  const Text(
-                                    "Login",
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 18),
-                                  ),
-                                  const SizedBox(
-                                    height: 16,
-                                  ),
-                                  TextFormField(
-                                    autovalidateMode: AutovalidateMode.always,
-                                    autofillHints: const [AutofillHints.email],
-                                    //not for form, this will make the input suggest that the field wants email as input
-                                    decoration: const InputDecoration(
-                                        fillColor: Colors.amber,
-                                        prefixIcon:
-                                            Icon(Icons.account_circle_outlined),
-                                        border: OutlineInputBorder(),
-                                        labelText: "Email/UserName ",
-                                        hintText: "example@gmail.com"),
+                      color: Colors.white,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                        child: Center(
+                          child: SizedBox(
+                            // color: Colors.amber,
+                            height: height / 2.5,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  "Login",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18),
+                                ),
+                                SizedBox(
+                                  height: height / 32,
+                                ),
+                                TextFormField(
+                                  autovalidateMode: AutovalidateMode.always,
+                                  autofillHints: const [AutofillHints.email],
+                                  //not for form, this will make the input suggest that the field wants email as input
+                                  decoration: const InputDecoration(
+                                      // fillColor: Colors.amber,
+                                      prefixIcon:
+                                          Icon(Icons.account_circle_outlined),
+                                      border: OutlineInputBorder(),
+                                      labelText: "Email/UserName ",
+                                      hintText: "example@gmail.com"),
 
-                                    cursorColor: Colors.red,
-                                  ),
-                                  const SizedBox(
-                                    height: 20,
-                                  ),
-                                  TextFormField(
-                                    autovalidateMode: AutovalidateMode.always,
-                                    autofillHints: const [AutofillHints.email],
-                                    //not for form, this will make the input suggest that the field wants email as input
-                                    decoration: const InputDecoration(
-                                        prefixIcon:
-                                            Icon(Icons.lock_outline_rounded),
-                                        border: OutlineInputBorder(),
-                                        labelText: "Password : ",
-                                        hintText: " type your password here"),
+                                  cursorColor: Colors.red,
+                                ),
+                                SizedBox(
+                                  height: height / 48,
+                                ),
+                                TextFormField(
+                                  autovalidateMode: AutovalidateMode.always,
+                                  autofillHints: const [AutofillHints.email],
+                                  //not for form, this will make the input suggest that the field wants email as input
+                                  decoration: const InputDecoration(
+                                      prefixIcon:
+                                          Icon(Icons.lock_outline_rounded),
+                                      border: OutlineInputBorder(),
+                                      labelText: "Password : ",
+                                      hintText: " type your password here"),
 
-                                    cursorColor: Colors.red,
-                                  ),
-                                  const SizedBox(
-                                    height: 20,
-                                  ),
-                                  InkWell(
-                                    onTap: () {
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                            builder: (context) =>
-                                                SidebarXExampleApp()),
-                                      );
-                                    },
-                                    child: Center(
-                                      child: Card(
-                                        elevation: 10,
-                                        shape: RoundedRectangleBorder(
-                                          borderRadius:
-                                              BorderRadius.circular(20),
-                                          //set border radius more than 50% of height and width to make circle
-                                        ),
-                                        child: const CardWidget(
-                                            gradient: [
-                                              Color.fromARGB(255, 211, 32, 39),
-                                              Color.fromARGB(255, 164, 92, 95)
-                                            ],
-                                            width: 340,
-                                            height: 48,
-                                            borderRadius: 13,
-                                            child: Center(
-                                              child: Text(
-                                                "Login",
-                                                style: TextStyle(
-                                                    fontWeight: FontWeight.bold,
-                                                    color: Colors.white),
-                                              ),
-                                            )),
+                                  cursorColor: Colors.red,
+                                ),
+                                SizedBox(
+                                  height: height / 46,
+                                ),
+                                InkWell(
+                                  onTap: () {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                          builder: (context) =>
+                                              SidebarXExampleApp()),
+                                    );
+                                  },
+                                  child: Center(
+                                    child: Card(
+                                      elevation: 10,
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(20),
+                                        //set border radius more than 50% of height and width to make circle
                                       ),
+                                      child: const CardWidget(
+                                          gradient: [
+                                            Color.fromARGB(255, 211, 32, 39),
+                                            Color.fromARGB(255, 164, 92, 95)
+                                          ],
+                                          width: 340,
+                                          height: 48,
+                                          borderRadius: 13,
+                                          child: Center(
+                                            child: Text(
+                                              "Login",
+                                              style: TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                  color: Colors.white),
+                                            ),
+                                          )),
                                     ),
-                                  )
-                                ],
-                              ),
+                                  ),
+                                )
+                              ],
                             ),
                           ),
                         ),
-                      ]),
+                      ),
                     ))
               ],
             ),
