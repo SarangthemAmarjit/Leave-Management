a5cd8b93daeb8612caf08ba7b8f874a8f0b1aa89
Pusparani1996 <heisnampusparani@gmail.com>
2023-04-12 10:53:48 +0530

    changes

--- lib/pages/sidebar.dart
+++ lib/pages/sidebar.dart
@@ -1,7 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:leavemanagementadmin/pages/allbranch.dart';
+import 'package:leavemanagementadmin/pages/branch.dart';
 import 'package:leavemanagementadmin/pages/department.dart';
 import 'package:leavemanagementadmin/pages/designation.dart';
 import 'package:leavemanagementadmin/pages/homepage.dart';
